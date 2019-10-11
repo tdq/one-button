30c5416a30e279a5d897d272cbafea5535cff782
Nikolai Gorokhov <nikolai@vaadin.com>
2019-10-11 18:13:26 +0300

    switch to svg

--- src/one-button.js
+++ src/one-button.js
@@ -6,10 +6,10 @@ class OneButton extends LitElement {
             <style>
                 :host {
                     display: block;
+                    position: relative;
                 }
                 .one-button {
                     display: block;
-                    border: 3px solid green;
                     border-radius: 50%;
                     width: 100%;
                     height: 100%;
@@ -22,8 +22,21 @@ class OneButton extends LitElement {
                     height: 100%;
                     text-align: center;
                 }
+
+                .rotator {
+                    width: 100%;
+                    height: 100%;
+                    position: absolute;
+                    left: 0;
+                    top: 0;
+                    pointer-events: none;
+                }
             </style>
             <div class="one-button" @click="${this._clickEvent}" @touch="${this._clickEvent}" @mousedown="${this._startRotate}" @touchstart="${this._startRotate}" @mousemove="${this._rotate}" @touchmove="${this._rotate}" @mouseup="${this._stopRotate}" @touchend="${this._stopRotate}"><slot></slot></div>
+            <svg class="rotator" viewBox="0 0 100 100">
+                <circle cx="50" cy="50" r="44" stroke-dasharray="5" stroke-width="5" fill="none" stroke="green"/>
+                <circle cx="50" cy="6" r="6" fill="green"/>
+            </svg>
         `;
     }
 
@@ -39,18 +52,24 @@ class OneButton extends LitElement {
     }
 
     _startRotate(event) {
-        this._rotateEnabled = true;
         const centerX = this.offsetLeft + this.clientWidth / 2;
         const centerY = this.offsetTop + this.clientHeight / 2;
         const relativeX = (event.type && event.type === 'touchstart' ? event.touches[0].pageX :  event.x) - centerX;
         const relativeY = (event.type && event.type === 'touchstart' ? event.touches[0].pageY :  event.y) - centerY;
         const relation = relativeY / relativeX;
+        const actualRadius = this.clientWidth / 2;
+        const clickRadius = Math.sqrt(relativeX*relativeX + relativeY*relativeY);
 
-        this.initialAngle =  (relativeX < 0 ? Math.PI + Math.atan(relation) : Math.atan(relation));
+        if(clickRadius > actualRadius - 15 && clickRadius <= actualRadius) {
+            //event.preventDefault();
+            this._rotateEnabled = true;
+            this.initialAngle =  (relativeX < 0 ? Math.PI + Math.atan(relation) : Math.atan(relation));
+        }
     }
 
     _rotate(event) {
         if(this._rotateEnabled) {
+            //event.preventDefault();
             const centerX = this.offsetLeft + this.clientWidth / 2;
             const centerY = this.offsetTop + this.clientHeight / 2;
 
@@ -63,12 +82,12 @@ class OneButton extends LitElement {
             this.initialAngle += delta;
             this.angle += delta;
 
-            this.shadowRoot.querySelector(".one-button").style.transform = "rotate("+ this.angle +"rad)";
-            
+            this.shadowRoot.querySelector(".rotator").style.transform = "rotate("+ this.angle +"rad)";
         }
     }
 
     _stopRotate(event) {
+        //event.preventDefault();
         this._rotateEnabled = false;
     }
 }
